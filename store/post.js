57eb20fb0f0b033c6cd56c0e729093633668c3be
青春 <qingchun@qq.com>
2021-09-17 01:16:28 +0800

    收藏功能完善

--- store/post.js
+++ store/post.js
@@ -1,8 +1,10 @@
 //数据存储
 export const state =() => ({
-  // 点赞状态
+  // 收藏状态
   isShow:false,
-  thisList:{}
+
+  // 收藏数据的数组
+  thisList:[]
   
 })
 //数据处理函数
@@ -10,11 +12,14 @@ export const mutations = {
  setShow(state,info){
    state.isShow=info
  },
- 
- thingsList(state,list){
-   state.thisList={...list}
- }
 
+ thingsList(state,lists){
+
+   state.thisList= [...lists]
+  },
+  clearAll(state){
+     state.thisList=[]
+  }
 }
 //异步执行请求
 export const actions = {
